f8026e7e260a47710854e07816f466ace69c0101
Sule <aybilenden@gmail.com>
2022-03-22 21:59:19 +0300

    tour places start

---
+++ src/components/Body.jsx
@@ -0,0 +1,23 @@
+import "../App.css";
+function Body({ veri }) {
+  // console.log(dataList);
+  return (
+    <section>
+      {veri.map(({ title, desc, id, image }) => {
+        return (
+          <div className="card" key={id}>
+            <h1>{title}</h1>
+            <nav>
+              <img src={image} alt="" />
+              <div className="overlay">
+                <div className="text">{desc}</div>
+              </div>
+            </nav>
+          </div>
+        );
+      })}
+    </section>
+  );
+}
+
+export default Body;
