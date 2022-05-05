655d96a5eb46d81c9826b8196b84275e0dd1f735
Sule <aybilenden@gmail.com>
2022-05-05 21:06:43 +0300

    Updates

---
+++ precache-manifest.fc6824c44871b81811106bd8105293be.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "14f10d8c08cb26c4e53c",
+    "url": "/populer-tour-places/static/css/main.48c17e7c.chunk.css"
+  },
+  {
+    "revision": "14f10d8c08cb26c4e53c",
+    "url": "/populer-tour-places/static/js/main.14f10d8c.chunk.js"
+  },
+  {
+    "revision": "c1307a6b0ccd05ac151f",
+    "url": "/populer-tour-places/static/js/1.c1307a6b.chunk.js"
+  },
+  {
+    "revision": "3a01ab254ba3002813d1",
+    "url": "/populer-tour-places/static/js/runtime~main.3a01ab25.js"
+  },
+  {
+    "revision": "d06e4ce30017771e454946a912b72504",
+    "url": "/populer-tour-places/index.html"
+  }
+];
